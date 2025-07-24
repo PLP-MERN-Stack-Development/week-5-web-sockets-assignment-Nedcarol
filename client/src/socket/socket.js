25367e58bac8aaf18e3cc752cee2064df8312d75
Nedcarol <neddysigey@gmail.com>
2025-07-24 11:06:23 +0300

    Week 5 modified

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 import { useEffect, useState } from 'react';
 
 // Socket.io connection URL
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5002';
+const SOCKET_URL = 'https://week-5-web-sockets-assignment-nedcarol.onrender.com';
 
 // Create socket instance
 export const socket = io(SOCKET_URL, {
